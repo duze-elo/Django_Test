c245353fdbe4058414f4fb51295dd12d0969c769
Kuba <meowmme@gmail.com>
2025-02-26 14:12:28 +0100

    add login/register

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import TodoItem
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
+@login_required
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
