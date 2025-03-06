7d409b407d8f084dfe35f5573844839177985c22
Kuba <meowmme@gmail.com>
2025-03-06 20:16:45 +0100

    add create_todo style

--- users/forms.py
+++ users/forms.py
@@ -4,32 +4,32 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Email'
     }))
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Username'
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Password'
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Confirm Password'
     }))
 
     class Meta:
-        model = User 
+        model = User
         fields = ['username', 'email', 'password1', 'password2']
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-            'class': 'form-control ', 
+            'class': 'form-control ',
             'placeholder': 'Username',
         }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Password',
         }))
